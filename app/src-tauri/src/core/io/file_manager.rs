e39b60142b2577473650a1c0e4e08f454058e380
DerLarso <srek@outlook.de>
2026-09-18 12:28:23 +0200

    fix: correct usage of HashAlgorithm for hash computation in FileManager

--- app/src-tauri/src/core/io/file_manager.rs
+++ app/src-tauri/src/core/io/file_manager.rs
@@ -7,7 +7,7 @@ use std::{
 use zip::{ZipArchive, ZipWriter, write::SimpleFileOptions};
 
 use crate::core::{
-    hash_manager::HashAlgorithm::Sha256, io::{directory_node::DirectoryNode, hashed_error::HashedError, hashed_file_struct::HashedFileStruct, meta_data::MetaData},
+    hash_manager::HashAlgorithm, io::{directory_node::DirectoryNode, hashed_error::HashedError, hashed_file_struct::HashedFileStruct, meta_data::MetaData},
 };
 
 pub struct FileManager {}
@@ -31,7 +31,7 @@ impl FileManager {
         serde_json::to_writer(&mut zip, meta)?;
 
         let tree = serde_json::to_vec(json)?;
-        let hash = Sha256.get_hash_from_bytes(&tree);
+        let hash = HashAlgorithm::Sha256.get_hash_from_bytes(&tree);
 
         zip.start_file("tree.json", options)?;
         zip.write_all(&tree)?;
@@ -44,26 +44,35 @@ impl FileManager {
     }
 
     pub fn open_file(&self, path: &str) -> Result<HashedFileStruct, HashedError> {
+
         if !Path::new(path).exists() {
             return Err(HashedError::FileNotFound(path.to_string()));
         }
+
         if !path.ends_with(".hashed") {
             return Err(HashedError::InvalidEnding(path.to_string()));
         }
+
         let file = File::open(path)?;
         let reader = BufReader::new(file);
+
         let mut content = ZipArchive::new(reader)?;
         let meta: MetaData = serde_json::from_reader(content.by_name("meta.json")?)?;
+
         let version = meta.get_version();
+        
         if version == 1 {
             let mut hash_of_tree = String::from("");
             content
                 .by_name("hash.sha256")?
                 .read_to_string(&mut hash_of_tree)?;
             let trimmed = hash_of_tree.trim().to_string();
+
             let mut s = Vec::new();
             content.by_name("tree.json")?.read_to_end(&mut s)?;
-            let hash = Sha256.get_hash_from_bytes(&s);
+
+            let hash = HashAlgorithm::Sha256.get_hash_from_bytes(&s);
+
             if trimmed == hash {
                 let dic: DirectoryNode = serde_json::from_slice(&s)?;
                 Ok(HashedFileStruct::new(meta,dic,hash))
